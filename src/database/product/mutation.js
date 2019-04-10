2c840bc4c8799e3e68493d3cdf9c8670123bf815
Bernardo Couto Silva <bernardocoutosilva@icloud.com>
2019-04-10 18:26:26 -0300

    Correção de mutation para criação de Product

--- src/database/product/mutation.js
+++ src/database/product/mutation.js
@@ -1,4 +1,4 @@
 export default `
-    CreateProduct(id: String!, name: String!, category: String!): Product @cypher(statement: "CREATE (p:Product { id: $id, name: $name }) MATCH (c:Category) WHERE c.name = $category MERGE (p)<-[:HAS_PRODUCT]-(c) RETURN p.id, p.name")
+    CreateProduct(id: String!, name: String!, category: String!): Product @cypher(statement: "CREATE (p:Product { id: $id, name: $name }) WITH p MATCH (c:Category) WHERE c.name = $category MERGE (p)<-[:HAS_PRODUCT]-(c) RETURN p, c")
     RemoveProduct(id: String!): Product @cypher(statement: "MATCH (p:Product) WHERE p.id = $id DETACH DELETE p")
 `
